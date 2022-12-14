e356c9f13731a42be7a5912f27e5ecf3488f14d9
Francisco Atia <franciscoatia2108@gmail.com>
2022-12-14 15:30:07 -0300

    bot trading 1.1.3

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from flask import Flask, jsonify
 from decouple import config
+from waitress import serve
 
 app = Flask(__name__)
 PORT = config('PORT')
 
 
-@app.route('/', methods=['GET'])
+@app.route('/hi', methods=['GET'])
 def index():
     return jsonify({'Message': 'Welcome to API REST tutorial'})
 
 
 if __name__ == '__main__':
-    app.run(port=PORT, debug=False)
+    serve(app, host='0.0.0.0', port=PORT, threads=2, url_prefix='/my-app')
