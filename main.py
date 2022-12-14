9c8de86d1d0e375065511aaee30bdb783cd47a22
Francisco Atia <franciscoatia2108@gmail.com>
2022-12-14 15:08:19 -0300

    bot trading 1.1.0

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from flask import Flask, jsonify
 
 app = Flask(__name__)
+PORT = 5000
 
 
 @app.route('/', methods=['GET'])
 def index():
     return jsonify({'Message': 'Welcome to API REST tutorial'})
+
+
+if __name__ == '__main__':
+    app.run(port=PORT, debug=False)
